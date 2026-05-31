dec0c0092da4b4cc6bc2f0e0d5d7d88e6db285c2
arturbarbuzano <arturbarbuzano@gmail.com>
2026-05-31 18:57:21 -0300

    Adicionando Front-End

---
+++ src/main/java/com/eventos/sistemaeventos/SistemaEventosApplication.java
@@ -0,0 +1,18 @@
+package com.eventos.sistemaeventos;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SistemaEventosApplication {
+
+
+public static void main(String[] args) {
+
+// Artur Costa Barbuzano, Henrique Abreu Hollanda da Rocha e Thiago Ferreira Nunes
+
+ SpringApplication.run(SistemaEventosApplication.class, args);
+ }
+}
--- src/main/java/com/eventos/sistemaeventos/entity/Categoria.java
+++ src/main/java/com/eventos/sistemaeventos/entity/Categoria.java
@@ -1,8 +1,14 @@
 package com.eventos.sistemaeventos.entity;
 
-import jakarta.persistence.*;
-
 import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity(name = "Categoria")
 @Table(name = "tb_categorias")
