441f0afb73e4bfb501fd633b7bdd2360b93b937e
ThiagoFerreiraNunes <thiagonflive@gmail.com>
2026-06-02 16:37:47 -0300

    finalizando o projeto

--- src/main/java/com/eventos/sistemaeventos/SistemaEventosApplication.java
+++ src/main/java/com/eventos/sistemaeventos/SistemaEventosApplication.java
@@ -8,11 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SistemaEventosApplication {
 
-
-public static void main(String[] args) {
-
+ public static void main(String[] args) {
 // Artur Costa Barbuzano, Henrique Abreu Hollanda da Rocha e Thiago Ferreira Nunes
-
- SpringApplication.run(SistemaEventosApplication.class, args);
+  SpringApplication.run(SistemaEventosApplication.class, args);
  }
 }
